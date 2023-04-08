8cb33f917f8e3dbe8b4604388872a4061cf2b304
Alex Kanunnikov <lifeart92@gmail.com>
2023-04-08 14:21:05 +0300

    detect video metadata load event in demo

--- demo/index.ts
+++ demo/index.ts
@@ -1,25 +1,31 @@
-new EventSource('/esbuild').addEventListener('change', () => location.reload())
+new EventSource("/esbuild").addEventListener("change", () => location.reload());
 
 import { SmAnnotate } from "../src";
 
 const video = document.querySelector("video") as HTMLVideoElement;
-const tool = new SmAnnotate(video);
 
-setInterval(() => {
-  tool.destroy();
-  tool.init(video);
-}, 100000000);
+video.addEventListener("loadedmetadata", () => {
+  // "The duration and dimensions of the media and tracks are now known."
 
-setInterval(() => {
-  console.log(tool.saveAllFrames());
-}, 100000);
+  requestAnimationFrame(() => {
+    // Video is ready to play
 
+    const tool = new SmAnnotate(video);
 
+    setInterval(() => {
+      tool.destroy();
+      tool.init(video);
+    }, 100000000);
 
+    setInterval(() => {
+      console.log(tool.saveAllFrames());
+    }, 100000);
 
-// setInterval(() => {
-//   const img = tool.imageForCapture();
-//   if (img) {
-//     document.body.appendChild(img);
-//   }
-// }, 10000);
+    // setInterval(() => {
+    //   const img = tool.imageForCapture();
+    //   if (img) {
+    //     document.body.appendChild(img);
+    //   }
+    // }, 10000);
+  });
+});
